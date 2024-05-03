34398c0e87d37b286b97b9f0d0163119428efa61
Justin <justin.rvd@gmail.com>
2024-05-03 02:23:20 -0500

    add functions to get the most repeated word

--- wordAnalyzer.py
+++ wordAnalyzer.py
@@ -12,3 +12,21 @@ class TextAnalyzer(object):
             freqMap[word] = wordList.count(word)   
         return freqMap
     
+    def orderByFrequency(self):
+        freqDict = self.freqAll()
+        sorted_dict_by_values = {k: v for k, v in sorted(freqDict.items(), key=lambda x: x[1], reverse=True)}
+        return sorted_dict_by_values
+    
+    def mostFrequentWord(self):
+        sorted = self.orderByFrequency()
+        keysIter = iter(sorted)
+        mostFrequent = next(keysIter)
+        frequency= sorted[mostFrequent]
+        return f"The most frequent is {mostFrequent} with the frequency of {frequency}"
+
+    def freqOf(self,word):
+        freqDict = self.freqAll()
+        if word in freqDict:
+            return freqDict[word]
+        else:
+            return 0
